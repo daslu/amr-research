577258fafb4bba0d3c0f317c0d799f54a42e3915
daslu <daniel.slutsky@gmail.com>
2025-11-18 17:19:11 +0200

    listed important bacteria

--- src/maldi/data/bacteria.clj
+++ src/maldi/data/bacteria.clj
@@ -1,10 +1,14 @@
 (ns maldi.data.bacteria)
 
 (def E-coli "Escherichia coli")
+(def S-aureus "Staphylococcus aureus")
+(def S-epidermidis "Staphylococcus epidermidis")
+(def P-aeruginosai "Pseudomonas aeruginosa")
+(def K-pneumoniae "Klebsiella pneumoniae")
 
-
-
-(def important-bacteria [])
-;; E. coli, S. aureus, S. epidermidis, P. aeruginosa ו־K. pneumoniae.
-
-
+(def important-bacteria
+  [E-coli
+   S-aureus
+   S-epidermidis
+   P-aeruginosai
+   K-pneumoniae])
